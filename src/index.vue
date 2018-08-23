4dcb6589b14bc5d713c72b3cfcfaa70c21a3885e
Melanie Downing <sayanything830@gmail.com>
2018-08-23 16:43:07 -0700

    first vue project

---
+++ src/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id='app'>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#app',
+  data: {
+    message: 'Hello World!'
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
